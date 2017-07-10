4a4d56e1b1d8724ef15c6bb96501101b5b037fe7
adurha10 <alamadu82@gmail.com>
2017-07-10 01:46:47 -0400

    changed to https

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -25,7 +25,7 @@ $("#submit-new-btn").on("click", function(event) {
 function searchButtonClick() {
       var subject = $(this).text();
 
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         subject + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       $.ajax({
